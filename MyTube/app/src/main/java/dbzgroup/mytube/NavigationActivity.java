3df5faaec926b39b463d54707a140a4686fdf319
Tai <taidao90@gmail.com>
2017-11-28 23:43:18 -0800

    Log out works

--- MyTube/app/src/main/java/dbzgroup/mytube/NavigationActivity.java
+++ MyTube/app/src/main/java/dbzgroup/mytube/NavigationActivity.java
@@ -1,22 +1,31 @@
 package dbzgroup.mytube;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class NavigationActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private Button mLogOutBtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
+            switch (item.getItemId()) { //Fragment Selector
                 case R.id.navigation_search:
                     mTextMessage.setText(R.string.title_search);
                     return true;
@@ -25,11 +34,19 @@ public class NavigationActivity extends AppCompatActivity {
                     return true;
                 case R.id.navigation_logout:
                     mTextMessage.setText(R.string.title_logout);
+                    mAuth.signOut();
                     return true;
             }
             return false;
         }
     };
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +56,16 @@ public class NavigationActivity extends AppCompatActivity {
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if(firebaseAuth.getCurrentUser() == null){
+                    startActivity(new Intent(NavigationActivity.this, SignIn.class));
+                }
+
+            }
+        };
+    }
 }
