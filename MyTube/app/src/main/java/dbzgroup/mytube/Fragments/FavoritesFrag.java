270431589acffe81795f41a651ac9b172bc5c2ab
Tai <taidao90@gmail.com>
2017-12-04 14:32:48 -0800

    bottomNavigationView stays bottom when keyboard

--- MyTube/app/src/main/java/dbzgroup/mytube/Fragments/FavoritesFrag.java
+++ MyTube/app/src/main/java/dbzgroup/mytube/Fragments/FavoritesFrag.java
@@ -2,11 +2,19 @@ package dbzgroup.mytube.Fragments;
 
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
+import java.util.List;
+
+import dbzgroup.mytube.Model.MyVideo;
+import dbzgroup.mytube.Model.VideoAdapter;
 import dbzgroup.mytube.R;
 
 /**
@@ -14,6 +22,10 @@ import dbzgroup.mytube.R;
  */
 public class FavoritesFrag extends Fragment {
     private View v;
+    private RecyclerView recyclerView;
+    private Handler handler;
+    private List<MyVideo> myVideoList;
+    private VideoAdapter adapter;
 
     public FavoritesFrag() {
         // Required empty public constructor
@@ -28,4 +40,9 @@ public class FavoritesFrag extends Fragment {
         return v;
     }
 
+    //When playlist is found/created put all videos in here...
+    private void updateVideosFound(){
+        adapter = new VideoAdapter(v.getContext(), myVideoList);
+        recyclerView.setAdapter(adapter);
+    }
 }
