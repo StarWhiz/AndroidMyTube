1e66f00093de3b812fe9ae2720f9fe1454083c11
Tai <taidao90@gmail.com>
2017-12-04 20:52:35 -0800

    I'm still stuck so frustrating so much struggling

--- MyTube/app/src/main/java/dbzgroup/mytube/Model/VideoAdapter.java
+++ MyTube/app/src/main/java/dbzgroup/mytube/Model/VideoAdapter.java
@@ -10,6 +10,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
@@ -31,6 +32,9 @@ import java.util.Calendar;
 
 import java.util.List;
 
+import com.prof.youtubeparser.Parser;
+import com.prof.youtubeparser.models.videos.Video;
+
 import dbzgroup.mytube.PlayerActivity;
 import dbzgroup.mytube.R;
 
@@ -44,6 +48,8 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
     private List<MyVideo> myVideoList;
     private static YouTube youtube; //global instance of youtube object
     private View view;
+    public static final String KEY = "AIzaSyAQcZqxCudMtxgUgpeSVR-xU292LAWWVXE";
+    private VideoViewHolder vholder;
 
     public VideoAdapter(Context mCtx, List<MyVideo> myVideoList) {
         this.mCtx = mCtx;
@@ -60,6 +66,7 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
     @Override
     public void onBindViewHolder(VideoViewHolder holder, int position) {
         MyVideo myVideo = myVideoList.get(position);
+        vholder = holder; //don't know if i will be needing this
         holder.videoTitle.setText(myVideo.getTitle());
         holder.videoPubDate.setText("Published Date " + myVideo.getPubDate());
         holder.videoViewCount.setText("Views " + myVideo.getNumberOfViews());
@@ -95,6 +102,8 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
                     if (!favorited) {
                         favButton.setImageResource(R.drawable.ic_favorite_black_24dp);
                         favorited = true;
+                        performPlaylistOP();
+                        System.out.println("hello! perform op!");
                     }
                     else {
                         favButton.setImageResource(R.drawable.ic_favorite_border_black_24dp);
@@ -120,6 +129,7 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
     }
 
     public void performPlaylistOP () {
+
         youtube = new YouTube.Builder(new NetHttpTransport(),
                 new JacksonFactory(), new HttpRequestInitializer() {
             @Override
@@ -127,32 +137,51 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
             }
         }).setApplicationName(view.getContext().getString(R.string.app_name)).build();
 
+        Parser parser = new Parser();
+
+        //(CHANNEL_ID, NUMBER_OF_RESULT_TO_SHOW, ORDER_TYPE ,BROSWER_API_KEY)
+        //https://www.youtube.com/channel/UCVHFbqXqoYvEWM1Ddxl0QDg --> channel id = UCVHFbqXqoYvEWM1Ddxl0QDg
+        //The maximum number of result to show is 50
+        //ORDER_TYPE --> by date: "Parser.ORDER_DATE" or by number of views: "ORDER_VIEW_COUNT"
+        String url = parser.generateRequest("PLEx_MIGn6OF6bCXYYoLG-XtKKVQbquAlh", 10, Parser.ORDER_DATE, KEY);
+        parser.execute(url);
+        parser.onFinish(new Parser.OnTaskCompleted() {
+
+            @Override
+            public void onTaskCompleted(ArrayList<Video> list, String nextPageToken) {
+                Picasso.with(mCtx).load(list.get(1).
+                //vholder.videoThumbnail
+                //what to do when the parsing is done
+                //the ArrayList contains all video data. For example you can use it for your adapter
+            }
+
+            @Override
+            public void onError() {
+                //what to do in case of error
+            }
+        });
+
+        /*
         try {
             HashMap<String, String> parameters = new HashMap<>();
-            parameters.put("part", "snippet,contentDetails");
-            parameters.put("mine", "true");
-            parameters.put("maxResults", "15");
+            parameters.put("part", "snippet,status");
             parameters.put("onBehalfOfContentOwner", "");
-            parameters.put("onBehalfOfContentOwnerChannel", "");
 
-            YouTube.Playlists.List playlistsListMineRequest = youtube.playlists().list(parameters.get("part").toString());
-            if (parameters.containsKey("mine") && parameters.get("mine") != "") {
-                boolean mine = (parameters.get("mine") == "true") ? true : false;
-                playlistsListMineRequest.setMine(mine);
-            }
 
-            if (parameters.containsKey("maxResults")) {
-                playlistsListMineRequest.setMaxResults(Long.parseLong(parameters.get("maxResults").toString()));
-            }
+            Playlist playlist = new Playlist();
+            PlaylistSnippet snippet = new PlaylistSnippet();
+            PlaylistStatus status = new PlaylistStatus();
+
+            playlist.setSnippet(snippet);
+            playlist.setStatus(status);
+
+            YouTube.Playlists.Insert playlistsInsertRequest = youtube.playlists().insert(parameters.get("part").toString(), playlist);
 
             if (parameters.containsKey("onBehalfOfContentOwner") && parameters.get("onBehalfOfContentOwner") != "") {
-                playlistsListMineRequest.setOnBehalfOfContentOwner(parameters.get("onBehalfOfContentOwner").toString());
+                playlistsInsertRequest.setOnBehalfOfContentOwner(parameters.get("onBehalfOfContentOwner").toString());
             }
 
-            if (parameters.containsKey("onBehalfOfContentOwnerChannel") && parameters.get("onBehalfOfContentOwnerChannel") != "") {
-                playlistsListMineRequest.setOnBehalfOfContentOwnerChannel(parameters.get("onBehalfOfContentOwnerChannel").toString());
-            }
-            PlaylistListResponse response = playlistsListMineRequest.execute();
+            Playlist response = playlistsInsertRequest.execute();
             System.out.println(response);
 
         } catch (GoogleJsonResponseException e) {
@@ -160,6 +189,6 @@ public class VideoAdapter extends RecyclerView.Adapter<VideoAdapter.VideoViewHol
             System.err.println("There was a service error: " + e.getDetails().getCode() + " : " + e.getDetails().getMessage());
         } catch (Throwable t) {
             t.printStackTrace();
-        }
+        }*/
     }
 }
